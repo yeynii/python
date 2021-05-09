c850d26c11b23a9875e3ba91f5d3d988d527c41f
yeynii <yeynii@gmail.com>
2021-05-09 17:48:48 +0900

    Update 3-2.py

--- greedy/3-2.py
+++ greedy/3-2.py
@@ -4,7 +4,7 @@ nums = list(map(int, input().split()))
 nums.sort()
 
 cnt1 = k * (int(m/(k+1))) + m % (k+1)
-cnt2 = int(m/(k+1))
+cnt2 = m - cnt1
 
 sum = cnt1 * nums[n-1] + cnt2 * nums[n-2]
 print(sum)
