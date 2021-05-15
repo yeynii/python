b8091730a4187065d861ab527173b6c9c640aeb6
yeynii <yeynii@gmail.com>
2021-05-16 05:36:49 +0900

    Update stack_2.py

--- stack_queue/stack_2.py
+++ stack_queue/stack_2.py
@@ -1,18 +1,10 @@
 def solution(progresses, speeds):
     answer = []
-    while len(progresses) > 0:
-        for i in range(len(progresses)):
-            progresses[i] += speeds[i]
-        cnt = 0
-        if progresses[0] >= 100:
-            while True:
-                if len(progresses) <= 0:
-                    break
-                if progresses[0] >= 100:
-                    cnt += 1
-                    progresses.pop(0)
-                    speeds.pop(0)
-                else: 
-                    break
-            answer.append(cnt)                      
+    Q = []
+    for p, s in zip(progresses, speeds):
+        if len(Q) == 0 or Q[-1][0] < -((p-100)//s):
+            Q.append([-((p-100)//s), 1])
+        else :
+            Q[-1][1] += 1
+    answer = [p[1] for p in Q]
     return answer
