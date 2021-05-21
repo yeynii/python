91396172d2b73b9b7b60568bc4163ca1ccf7a5f2
yeynii <yeynii@gmail.com>
2021-05-21 23:23:18 +0900

    Update sort_1.py

--- sort/sort_1.py
+++ sort/sort_1.py
@@ -7,12 +7,8 @@ def my_compare(x,y):
         return 1
     
 def solution(numbers):
-    str_numbers = []
-    for e in numbers:
-        str_numbers.append(str(e))
-    
+    str_numbers = list(map(str, numbers))
     if all(s == '0' for s in str_numbers):
         return '0'
-    
     str_numbers = sorted(str_numbers, key= cmp_to_key(my_compare))
     return "".join(str_numbers)
