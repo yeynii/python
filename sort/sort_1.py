e1193dcd3f1e4b4672e49c8f9b62483c8272ed06
yeynii <yeynii@gmail.com>
2021-05-21 23:12:33 +0900

    Create sort_1.py

---
+++ sort/sort_1.py
@@ -0,0 +1,18 @@
+from functools import cmp_to_key
+
+def my_compare(x,y):
+    if int(x + y) > int(y + x):
+        return -1
+    else:
+        return 1
+    
+def solution(numbers):
+    str_numbers = []
+    for e in numbers:
+        str_numbers.append(str(e))
+    
+    if all(s == '0' for s in str_numbers):
+        return '0'
+    
+    str_numbers = sorted(str_numbers, key= cmp_to_key(my_compare))
+    return "".join(str_numbers)
