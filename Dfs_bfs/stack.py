1ff211766481749ceb0f191d3a02a9cc660af91b
yeynii <yeynii@gmail.com>
2021-06-30 21:09:40 +0900

    Create stack.py

---
+++ Dfs_bfs/stack.py
@@ -0,0 +1,12 @@
+stack = []
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+stack.pop()
+stack.append(5)
+stack.append(6)
+stack.pop()
+
+print(stack) # 최하단 원소부터 출력
+print(stack[::-1]) # 최상단 원소부터 출력
